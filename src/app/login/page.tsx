9b212a62e6616cc32b22e1fa88fc98e3d31680d6
yubnee31 <tydbqls@gmail.com>
2024-01-08 17:59:23 +0900

    Feat: 로그인기능구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,54 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { googleLogin, kakaoLogin, logOut } from '@/api/auth';
 
-const login = () => {
+const Login = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [isValid, setIsValid] = useState<boolean>(false);
+
+  const clickLoginButtonHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const handleEmailInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const email = e.target.value;
+    setEmail(email);
+    email.includes('@') && password.length >= 5
+      ? setIsValid(true)
+      : setIsValid(false);
+  };
+
+  const handlePasswordInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const password = e.target.value;
+    setPassword(password);
+    email.includes('@') && password.length >= 5
+      ? setIsValid(true)
+      : setIsValid(false);
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={clickLoginButtonHandler}>
         <p>이메일</p>
-        <input placeholder='이메일을 입력해주세요'></input>
+        <input
+          placeholder='이메일을 입력해주세요'
+          value={email}
+          onChange={handleEmailInput}
+        ></input>
         <p>비밀번호</p>
-        <input placeholder='비밀번호를 입력해주세요'></input>
+        <input
+          type='password'
+          placeholder='비밀번호를 5자 이상으로 입력해주세요'
+          value={password}
+          onChange={handlePasswordInput}
+          minLength={5}
+        ></input>
         <div>
-          <button>로그인</button>
+          <button type='submit' disabled={!isValid}>
+            로그인
+          </button>
           <button onClick={googleLogin}>Google로 로그인하기</button>
           <button onClick={kakaoLogin}>kakao로 로그인하기</button>
           <button onClick={logOut}>로그아웃</button>
@@ -22,4 +58,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default Login;
