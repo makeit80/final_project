f5d47e5f6f77c8ff5c419bf66cca4d523d82847a
YoungJ <wns4011@daum.net>
2024-01-09 10:04:28 +0900

    좋아요 기능 작업 브랜치 재생성

---
+++ src/components/layout/Provider.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const provider = ({ children }: Props) => {
+  const client = new QueryClient();
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default provider;
